02fc82a39c00b7960f12ac3f13325a00400b18dd
Luiz Casara <luiz.casara@maxxidata.com>
2023-07-29 21:21:24 -0300

    env

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 require("dotenv").config();
 const nextConfig = {
-  // env: {
-  //   GOOGLE_KEY: "AIzaSyD81jOdjhyWW2jQbRxCNRe0deridnHE0To",
-  // },
+  env: {
+    GOOGLE_KEY: "AIzaSyD81jOdjhyWW2jQbRxCNRe0deridnHE0To",
+  },
 };
 
 module.exports = nextConfig;
